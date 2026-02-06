0f50323a5fd23e321f6daf7dbaf72e69f5181a18
Allan <allanbarboza.git@gmail.com>
2026-02-06 18:47:32 -0300

    chore: :globe_with_meridians: add validation for pt-br

---
+++ resources/lang/pt_BR/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'required' => 'O campo :attribute é obrigatório.',
+    'unique'   => 'Este :attribute já está em uso.',
+    'min' => [
+        'string' => 'O campo :attribute deve ter no mínimo :min caracteres.',
+    ],
+
+    'attributes' => [
+        'username' => 'nome de usuário',
+        'password' => 'senha',
+        'name'     => 'nome',
+    ],
+];
